e5e34b1fca74c10ec61f87ad328917ce0148bcff
iganb <2775038426@qq.com>
2023-12-11 15:09:18 +0800

    完成了首页功能的开发（轮播图，分类，楼层）

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 import Vue from 'vue'
 import App from './App'
+// 按需导入 $http 对象
+import { $http } from '@escook/request-miniprogram'
+
+uni.$http = $http
+//请求根路径
+$http.baseUrl = 'https://www.uinav.com'
+
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title:"数据加载中"
+  })
+}
+$http.beforeRequest = function (options) {
+  uni.hideLoading()
+}
+
+// 封装的展示消息提示的方法
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
 
 Vue.config.productionTip = false
 
