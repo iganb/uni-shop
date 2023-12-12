241768421db4f524f0fdfc85047e009b5e17033f
iganb <2775038426@qq.com>
2023-12-12 16:36:30 +0800

    完成了搜索功能的开发

--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,5 +1,7 @@
 <template>
    <view>
+   <!-- <my-search :bgcolor="'pink'" :radius="'3'"></my-search> -->
+    <my-search @click="gotoSearch"></my-search>
       <view class="scroll-view-container">
         <!-- 左侧的滚动视图区域 -->
         <scroll-view class="left-scroll-view" scroll-y :style="{height: wh + 'px'}">
@@ -46,7 +48,7 @@
       // 获取当前系统的信息
       const sysInfo = uni.getSystemInfoSync()
       // 为 wh 窗口可用高度动态赋值
-      this.wh = sysInfo.windowHeight
+      this.wh = sysInfo.windowHeight -50
       this.getCateList()
     },
     methods : {
@@ -75,7 +77,13 @@
       uni.navigateTo({
         url: '/subpkg/goods_list/goods_list?cid=' + item3.cat_id
       })
-    }
+    },
+     // 跳转到分包中的搜索页面
+       gotoSearch() {
+         uni.navigateTo({
+           url: '/subpkg/search/search'
+         })
+       }
     }
   }
 </script>
@@ -125,6 +133,7 @@
     flex-wrap: wrap;
   
     .cate-lv3-item {
+      
       width: 33.33%;
       margin-bottom: 10px;
       display: flex;
@@ -141,5 +150,6 @@
       }
     }
   }
+  
 }
 </style>
